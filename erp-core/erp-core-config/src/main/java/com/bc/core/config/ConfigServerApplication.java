2bd1d824e434ce2149e7284b93773b3e73cc2f67
BooksCup <812809680@qq.com>
2021-04-21 12:03:29 +0800

    first commit

---
+++ erp-core/erp-core-config/src/main/java/com/bc/core/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.bc.core.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 配置
+ *
+ * @author zhou
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
---
+++ erp-core/erp-core-eureka/src/main/java/com/bc/core/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.bc.core.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * eureka
+ *
+ * @author zhou
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
